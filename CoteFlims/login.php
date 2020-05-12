a15a47cf2f86d5815ed7ee3555f1a53bc541feae
andres david marin hernandez <31189114+descargalo17@users.noreply.github.com>
2020-05-12 00:35:53 -0500

    Add files via upload

--- CoteFlims/login.php
+++ CoteFlims/login.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   
     <!-- icons -->
+    <link rel="stylesheet" href="estilo.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" integrity="sha384-v8BU367qNbs/aIZIxuivaU55N5GPF89WBerHoGA4QTcbUjYiLQtKdrfXnqAcXyTv" crossorigin="anonymous">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -40,8 +41,25 @@
             </nav>
 
 
+
+
             
         </nav>
+
+
+        
+        <div class="login">
+	<h1>INICIAR SESION</h1>
+    <form method="post">
+      <input type="text" name="u" placeholder="USUARIO" required="required" /><br>
+      <br>
+        <input type="password" name="p" placeholder="CONTRASEÑA" required="required" />
+        <br>
+        <br>
+        <button type="submit" class="btn btn-primary btn-block btn-large">INICIAR SESION</button>
+    </form>
+</div>
+
         
     
 
